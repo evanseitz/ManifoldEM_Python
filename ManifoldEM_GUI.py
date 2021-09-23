80c1df36a787fdb76645543ab3bb39a6b7f049a6
Evan Seitz <evanseitz@Kaitlins-iMac.local>
2021-09-23 13:30:24 -0400

    update

--- ManifoldEM_GUI.py
+++ ManifoldEM_GUI.py
@@ -7055,7 +7055,7 @@ class Manifold3dCanvas(QtGui.QDialog):
         #self.toolbar = NavigationToolbar(Manifold3dCanvas.canvas, self)
         self.ax = Axes3D(self.figure)
         self.ax.mouse_init()
-        self.ax.view_init(90, 90)
+        self.ax.view_init(90, -90)
 
         # Matplotlib Default Colors:
         # ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
--- data_viewers/DM_viewer.py
+++ data_viewers/DM_viewer.py
@@ -1,9 +1,6 @@
 import os
-#from pyface.qt import QtGui, QtCore
-#os.environ['ETS_TOOLKIT'] = 'qt4'
+import shutil
 import matplotlib
-#from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-#from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 from matplotlib.widgets import Slider, Button
 import mpl_toolkits.axes_grid1
@@ -29,69 +26,138 @@ from matplotlib import cm
 # Copyright (c) Columbia University Evan Seitz 2020
 #################################################################################
 
-projName = 'untitled'
-parDir = os.path.abspath('..')
-dmDir = os.path.join(parDir, 'outputs_%s/diff_maps' % (projName))
-PD = 1 #current PD (zero indexing)
-
 if 1: #render with LaTeX font for figures
     rc('text', usetex=True)
     rc('font', family='serif')
+    
+# user parameters:
+projName = 'Apt5'
+PD = 7 #current PD (zero indexing)
+Batch = True #if True, batch print all PD embeddings to files in current directory
 
-if 1: #plot of all eigenfunctions v1 (up to 'dimRows/Cols') vs all others (v1+i)
-    fname = open(os.path.join(dmDir,'gC_trimmed_psi_prD_%s' % (PD)), 'rb')
-    data = pickle.load(fname)
-    DM = data['psi']
-    
-    s = 20
-    lw = .5
-    idx = 1
-    print(np.shape(DM))
-    m = np.shape(DM)[0]
-    enum = np.arange(1,m+1)
-    fig = plt.figure() 
-    dimRows = 6 #4
-    dimCols = 8 #5
+# don't change:
+pyDir = os.path.dirname(os.path.abspath(__file__)) #python file location
+parDir = os.path.abspath('..')
+dmDir = os.path.join(parDir, 'outputs_%s/diff_maps' % (projName))
 
-    for v1 in range(1,dimRows+1):
-        for v2 in range(v1, v1+dimCols):
-            plt.subplot(dimRows, dimCols, idx)
-            plt.scatter(DM[:,v1-1], DM[:,v2-1], s=s, edgecolor='k', linewidths=.1, zorder=1)
-            plt.xlabel(r'$\Psi_{%s}$' % (v1), fontsize=12, labelpad=5)
-            plt.ylabel(r'$\Psi_{%s}$' % (v2), fontsize=12, labelpad=2.5)
-            plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-            plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0)) 
-            plt.rc('font', size=6)
-            if 1:
-                frame = plt.gca()
-                frame.axes.xaxis.set_ticklabels([])
-                frame.axes.yaxis.set_ticklabels([])
-                plt.gca().set_xticks([])
-                plt.xticks([])
-                plt.gca().set_yticks([])
-                plt.yticks([])
-            else:
-                plt.tick_params(axis="x", labelsize=6)
-                plt.tick_params(axis="y", labelsize=6) 
-            idx += 1 
-    plt.tight_layout()
-    plt.subplots_adjust(left=0.03, bottom=0.03, right=0.97, top=0.97, wspace=0.25, hspace=0.25)
-    plt.show()
+if Batch is False: #standard use (view one PD embedding at a time)
+    if 1: #plot of all eigenfunctions v1 (up to 'dimRows/Cols') vs all others (v1+i)
+        fname = open(os.path.join(dmDir,'gC_trimmed_psi_prD_%s' % (PD)), 'rb')
+        data = pickle.load(fname)
+        DM = data['psi']
+        
+        s = 20
+        lw = .5
+        idx = 1
+        print(np.shape(DM))
+        m = np.shape(DM)[0]
+        enum = np.arange(1,m+1)
+        fig = plt.figure() 
+        dimRows = 6 #4
+        dimCols = 8 #5
     
-if 0: #3d diffusion map
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    v1 = 2
-    v2 = 3
-    v3 = 4
-    ax.scatter(DM[:,v1], DM[:,v2], DM[:,v3], c=enum, cmap='gist_rainbow', linewidths=lw, s=s, edgecolor='k')    
-    ax.set_xlabel('psi %s' % v1)
-    ax.set_ylabel('psi %s' % v2)
-    ax.set_zlabel('psi %s' % v3)
-    ax.view_init(elev=-90, azim=90)
-    plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-    plt.ticklabel_format(style='sci', axis='z', scilimits=(0,0))
-    plt.show()
+        for v1 in range(1,dimRows+1):
+            for v2 in range(v1, v1+dimCols):
+                plt.subplot(dimRows, dimCols, idx)
+                plt.scatter(DM[:,v1-1], DM[:,v2-1], s=s, edgecolor='k', linewidths=.1, zorder=1)
+                plt.xlabel(r'$\Psi_{%s}$' % (v1), fontsize=12, labelpad=5)
+                plt.ylabel(r'$\Psi_{%s}$' % (v2), fontsize=12, labelpad=2.5)
+                plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+                plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0)) 
+                plt.rc('font', size=6)
+                if 1:
+                    frame = plt.gca()
+                    frame.axes.xaxis.set_ticklabels([])
+                    frame.axes.yaxis.set_ticklabels([])
+                    plt.gca().set_xticks([])
+                    plt.xticks([])
+                    plt.gca().set_yticks([])
+                    plt.yticks([])
+                else:
+                    plt.tick_params(axis="x", labelsize=6)
+                    plt.tick_params(axis="y", labelsize=6) 
+                idx += 1 
+        plt.tight_layout()
+        plt.subplots_adjust(left=0.03, bottom=0.03, right=0.97, top=0.97, wspace=0.25, hspace=0.25)
+        plt.show()
+        
+    if 0: #3d diffusion map
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        v1 = 0
+        v2 = 1
+        v3 = 2
+        ax.scatter(DM[:,v1], DM[:,v2], DM[:,v3], c=enum, cmap='gist_rainbow', linewidths=lw, s=s, edgecolor='k')    
+        ax.set_xlabel('psi %s' % v1)
+        ax.set_ylabel('psi %s' % v2)
+        ax.set_zlabel('psi %s' % v3)
+        ax.view_init(elev=-90, azim=90)
+        plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        plt.ticklabel_format(style='sci', axis='z', scilimits=(0,0))
+        plt.show()
     
-#plt.axis('scaled')
+    
+else: #batch print all PD embeddings to files in current directory
+
+    # determine total number of PDs:
+    toposDir = os.path.join(parDir, 'outputs_%s/topos' % (projName))
+    PDs = 0
+    for root, dirs, files in os.walk(toposDir):
+        for file in sorted(files):
+            if not file.startswith('.'): #ignore hidden files
+                if file.startswith('class_avg'):
+                    PDs += 1
+    print('total PDs:', PDs)
+
+    # ready figure directory
+    outDir = os.path.join(pyDir, 'Embeddings_%s' % projName)
+    if os.path.exists(outDir):
+        shutil.rmtree(outDir)
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+
+    for PD in range(0,PDs):
+        fname = open(os.path.join(dmDir,'gC_trimmed_psi_prD_%s' % (PD)), 'rb')
+        data = pickle.load(fname)
+        DM = data['psi']
+        
+        s = 20
+        lw = .5
+        idx = 1
+        print(PD+1, np.shape(DM)[0])
+        m = np.shape(DM)[0]
+        enum = np.arange(1,m+1)
+        fig = plt.figure() 
+        dimRows = 6 #4
+        dimCols = 6 #5
+        plt.suptitle('PD: %s | Occupancy: %s' % (PD+1, np.shape(DM)[0]))
+    
+        for v1 in range(1,dimRows+1):
+            for v2 in range(v1, v1+dimCols):
+                plt.subplot(dimRows, dimCols, idx)
+                plt.scatter(DM[:,v1-1], DM[:,v2-1], s=s, edgecolor='k', linewidths=.1, zorder=1)
+                plt.xlabel(r'$\Psi_{%s}$' % (v1), fontsize=8, labelpad=2)
+                plt.ylabel(r'$\Psi_{%s}$' % (v2), fontsize=8, labelpad=2)
+                plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+                plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0)) 
+                plt.rc('font', size=6)
+                if 1:
+                    frame = plt.gca()
+                    frame.axes.xaxis.set_ticklabels([])
+                    frame.axes.yaxis.set_ticklabels([])
+                    plt.gca().set_xticks([])
+                    plt.xticks([])
+                    plt.gca().set_yticks([])
+                    plt.yticks([])
+                else:
+                    plt.tick_params(axis="x", labelsize=6)
+                    plt.tick_params(axis="y", labelsize=6) 
+                idx += 1
+                #plt.axis('scaled')
+        plt.tight_layout()
+        #plt.subplots_adjust(left=0.03, bottom=0.03, right=0.97, top=0.97, wspace=0.25, hspace=0.25)
+        fig = plt.gcf()
+        fig.savefig(os.path.join(outDir,'Embedding_%s.png' % (PD+1)), dpi=200)
+        #plt.show()
+        plt.clf()
